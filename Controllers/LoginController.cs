40514a5e491596640fbf51a5be605072018d7e29
Carlos Franco <cdf.175@gmail.com>
2023-09-27 10:55:31 -0300

    Construyendo login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Data.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TpIntegradorSofttekFront.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		public IActionResult Ingresar(LoginDto dto) 
+		{
+			return View("~/Views/Home/Index.cshtml");
+		}
+	}
+}
