6582621d0acbbea5d74ff6e6400914cb24c88606
Carlos Franco <cdf.175@gmail.com>
2023-09-28 20:35:26 -0300

    Imagen y logo agregados

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using Data.Base;
+using Data.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TpIntegradorSofttekFront.Models;
+
+namespace TpIntegradorSofttekFront.Controllers
+{
+	public class ServiceController : Controller
+	{
+		private readonly IHttpClientFactory _httpClient;
+		public ServiceController(IHttpClientFactory httpClient)
+		{
+			_httpClient = httpClient;
+		}
+		public IActionResult index()
+		{
+			return View();
+		}
+
+	}
+}
